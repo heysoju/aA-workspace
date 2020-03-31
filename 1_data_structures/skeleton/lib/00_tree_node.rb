dc70ac95141c7f99ef129e9f44bf13f81f7c59d1
aykevy <kevysoju@gmail.com>
2020-03-30 21:22:34 -0500

    DFS, BFS

--- 1_data_structures/skeleton/lib/00_tree_node.rb
+++ 1_data_structures/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,52 @@
 class PolyTreeNode
 
+    attr_accessor :value, :parent, :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(parent)
+        @parent.children.delete(self) unless @parent.nil?
+        @parent = parent
+        parent.children << self unless parent.nil? || parent.children.include?(self)
+    end
+
+    def add_child(child)
+        child.parent = self
+        @children << child unless @children.include?(child)
+    end
+
+    def remove_child(child)
+        child.parent = nil
+        if @children.include?(child)
+            @children.delete(child)
+        else
+            raise "Given child name is not a child of this node."
+        end
+    end
+
+    def dfs(target)
+        return self if @value == target
+        @children.each do | node |
+            value = node.dfs(target)
+            return value unless value.nil?
+        end
+        nil
+    end
+
+    def bfs(target)
+        queue = [self]
+        until queue.empty?
+            node = queue.shift
+            if node.value == target
+                return node
+            else
+                queue += node.children
+            end
+        end
+        nil
+    end
 end
