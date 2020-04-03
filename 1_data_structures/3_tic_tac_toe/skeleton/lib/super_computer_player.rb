91729ec612183109c33d831e8af5ef5601f44bcb
aykevy <kevysoju@gmail.com>
2020-04-02 22:50:28 -0500

    TTT completed

--- 1_data_structures/3_tic_tac_toe/skeleton/lib/super_computer_player.rb
+++ 1_data_structures/3_tic_tac_toe/skeleton/lib/super_computer_player.rb
@@ -2,6 +2,23 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    node.children.each do | n |
+      if n.winning_node?(mark)
+        return n.prev_move_pos # this is the move made in that baord
+      end
+    end
+
+    node.children.each do | n |
+      unless n.losing_node?(mark)
+        return n.prev_move_pos # this is the move made in that board
+      end
+    end
+
+    raise "There are no non-losing nodes"
+
+
+
   end
 end
 
--- 1_data_structures/3_tic_tac_toe/skeleton/lib/tic_tac_toe_node.rb
+++ 1_data_structures/3_tic_tac_toe/skeleton/lib/tic_tac_toe_node.rb
@@ -1,17 +1,56 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_accessor :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
+    if @board.over?
+      return @board.winner != evaluator && !@board.winner.nil?
+    end
+
+    if @next_mover_mark == evaluator
+      self.children.all? { | c | c.losing_node?(evaluator)}
+    else
+      self.children.any? { | c | c.losing_node?(evaluator)}
+    end
+   
   end
 
   def winning_node?(evaluator)
+    if @board.over?
+      return @board.winner == evaluator
+    end
+
+    if @next_mover_mark == evaluator
+      self.children.any? { | c | c.winning_node?(evaluator) }
+    else
+      self.children.all? { | c | c.winning_node?(evaluator) }
+    end
+    
+    
   end
 
-  # This method generates an array of all moves that can be made after
-  # the current move.
+  
   def children
+    moves = []
+    (0..2).each do | row_idx |
+      (0..2).each do | col_idx |
+        pos = [row_idx, col_idx]
+        if @board.empty?(pos)
+          new_board = @board.dup
+          new_board[pos] = @next_mover_mark
+          mark = @next_mover_mark == :x ? :o : :x
+          moves << TicTacToeNode.new(new_board, mark, pos)
+        end
+      end
+    end
+    moves
   end
+
 end
